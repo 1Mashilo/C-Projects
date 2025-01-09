9f3f28cb44e1fee661745c668aa64b4ffc7cebe3
1mashilo <christophermashilo@gmail.com>
2025-01-09 10:15:00 +0200

    add tree structure definition and initial implementation

--- database-server/tree.h
+++ database-server/tree.h
@@ -1,1 +1,24 @@
-/*tree.h*/
+#ifndef TREE_H
+#define TREE_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#ifdef __unix__
+#include <unistd.h>
+#endif
+#include <assert.h>
+#include <errno.h>
+#include <stdint.h>
+   
+typedef uint16_t uint16;
+typedef uint8_t uint8;
+
+struct Node {
+    struct Node *north;
+    struct Node *west;
+    struct Leaf *east;
+    uint8 path[256];
+};
+
+#endif // TREE_H
