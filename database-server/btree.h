0f7b63a672dff6aeefe0923e081e510164007028
1Mashilo <christophermashilo@gmail.com>
2025-01-15 19:03:04 +0200

    enhance btree.h with additional function prototypes and include stdio.h for file operations

--- database-server/btree.h
+++ database-server/btree.h
@@ -1,16 +1,23 @@
 #ifndef BTREE_H
 #define BTREE_H
 
+#include <stdio.h> // Include stdio.h for FILE type
+
+// B-tree node structure
 typedef struct BTreeNode {
-    int key;
-    struct BTreeNode *left;
-    struct BTreeNode *right;
+    int key;                // Key value
+    struct BTreeNode *left; // Pointer to the left child
+    struct BTreeNode *right; // Pointer to the right child
 } BTreeNode;
 
+// Function prototypes
 BTreeNode* createNode(int key);
 BTreeNode* insertNode(BTreeNode* root, int key);
-BTreeNode* deleteNode(BTreeNode* root, int key);
 BTreeNode* searchNode(BTreeNode* root, int key);
+BTreeNode* deleteNode(BTreeNode* root, int key);
+BTreeNode* minValueNode(BTreeNode* node);
+void saveTree(BTreeNode* root, FILE* file);
+BTreeNode* loadTree(FILE* file);
 void freeTree(BTreeNode* root);
 
 #endif // BTREE_H
